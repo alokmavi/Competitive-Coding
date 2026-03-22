11da0a72c6da5be50996bce8f4bc3fee37e77ed4
Alok Kumar Mavi <alokkumarmavi007@gmail.com>
2026-03-22 23:15:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fibonacci Number/fibonacci-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int n;
+    cin >> n;
+    
+    int a = 0;
+    int b = 1;
+    int c = 0;
+    
+    for (int i = 1; i <= n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    cout << a;
+    return 0;
+}
