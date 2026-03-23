e43a9365b80bc3849dcc87e181315dfaef51ffb6
Alok Kumar Mavi <alokkumarmavi007@gmail.com>
2026-03-23 23:46:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Statement/print-statement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+  public:
+    void utility() {
+        string s;
+        getline(cin, s);
+        cout << s << endl;
+    }
+};
