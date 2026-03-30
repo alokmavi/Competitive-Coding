bb9743b7b862c8d7e26ed5d424e3a1e849763d23
Alok Kumar Mavi <alokkumarmavi007@gmail.com>
2026-03-30 23:58:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Type Conversion/type-conversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double d;
+    cin >> d;
+    // Typecast and Print
+    int a = static_cast<int>(d);
+    cout << a;
+
+    return 0;
+}
