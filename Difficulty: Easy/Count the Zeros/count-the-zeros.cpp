6ae51b45433342688f2e88915258d12ecc79de8d
Alok Kumar Mavi <alokkumarmavi007@gmail.com>
2026-04-06 23:58:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count the Zeros/count-the-zeros.cpp
@@ -0,0 +1,16 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int countZeroes(vector<int> &arr) {
+        // code here
+        int count = 0;
+        
+        for(int x: arr){
+            if (x == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
