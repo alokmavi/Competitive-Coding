83e83ddfb59bb3c3e7c077462fff736a4290de4e
Alok Kumar Mavi <alokkumarmavi007@gmail.com>
2026-04-28 23:53:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Prime Number/next-prime-number.cpp
+++ Difficulty: Medium/Next Prime Number/next-prime-number.cpp
@@ -1,18 +1,27 @@
 // User function Template for C++
 
-int nextPrime(int n) {
+bool isPrime(int num) {
 
     // code here to find next prime number
     // return next prime number
-    n++;
-    while (true){
-        int i;
-        for (i = 2; i < n; i++){
-            if (n%i == 0) break;
+    if (num <= 1) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false; //even numbers greater than 2 are not prime
+    
+    // Check for odd divisors from 3 up to sqrt(num)
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int nextPrime(int n) {
+    int candidate = n + 1;
+    
+    while (true) {
+        if (isPrime(candidate)) {
+            return candidate;
         }
-        if (n == i){
-            return n;
-        }
-        n++;
+        candidate++;
     }
 }
