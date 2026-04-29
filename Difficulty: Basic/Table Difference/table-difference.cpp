a97083c759833bdb958033936fdf65e0c1f9c2a7
Alok Kumar Mavi <alokkumarmavi007@gmail.com>
2026-04-29 23:59:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Table Difference/table-difference.cpp
+++ Difficulty: Basic/Table Difference/table-difference.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
-using namespace std;
+// User function Template for C++
 
-int main() {
-    // code here
-    int n1, n2;
-    cin >> n1 >> n2;
+void difference(int n1, int n2) {
+
+    // Write your code here
+    int diff = n1 - n2;
     
-    for (int i = 1; i <= 10; i++){
-        int t1 = n1 * i;
-        int t2 = n2 * i;
-        cout << t1 - t2 << " ";
+    // Loop from 1 to 10 to print the first 10 multiples of the difference
+    for (int i = 1; i <= 10; ++i) {
+        cout << i * diff;
+        
+        // Print a space after the number, but not after the last one
+        if (i < 10) {
+            cout << " ";
+        }
     }
-
-    return 0;
 }
