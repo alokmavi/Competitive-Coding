b255365d643949d1e28537b7f365d5432853da96
Alok Kumar Mavi <alokkumarmavi007@gmail.com>
2026-05-03 23:54:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Struct area/struct-area.cpp
@@ -0,0 +1,26 @@
+/*struct area
+{
+    int sd;
+    int len, wid;
+}*/
+void find_area(int side, int le, int wd) {
+    // assign value to side of square
+    // your code here
+    
+
+    // assign values to length and breadth of rectangle
+    
+    // your code here
+
+    
+    // computing area of square
+    // your code here
+    int areaSquare = side * side;
+    
+
+    // computing area of rectangle
+    // your code here
+    int areaRectangle = le * wd;
+    
+    cout << areaSquare << " " << areaRectangle;
+}
